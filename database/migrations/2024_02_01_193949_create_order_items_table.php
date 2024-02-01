8c5eddf660519dfdeb09598c3e3b943b21d514b3
mfurkandogan <mfrkndgn@gmail.com>
2024-02-01 22:59:57 +0300

    initial commit

---
+++ database/migrations/2024_02_01_193949_create_order_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->bigInteger('product_id');
+            $table->integer('quantity');
+            $table->double('unitPrice',8,2);
+            $table->double('total',8,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+}
